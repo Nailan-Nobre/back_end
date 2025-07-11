258c86c512636b19e447fe0fabdb8386913c33a0
Nailan Nobre <nailannobre22@gmail.com>
2025-07-11 11:15:40 -0300

    ...

--- src/routes/agendamentoRoutes.js
+++ src/routes/agendamentoRoutes.js
@@ -8,8 +8,6 @@ router.post('/', authenticate, agendamentoController.criarAgendamento);
 router.get('/meus-agendamentos', authenticate, agendamentoController.listarAgendamentosUsuario);
 router.get('/pendentes', authenticate, agendamentoController.listarSolicitacoesManicure);
 router.get('/confirmados', authenticate, agendamentoController.listarAgendamentosConfirmados);
-router.get('/em-andamento', authenticate, agendamentoController.listarAgendamentosEmAndamento);
-router.get('/concluidos', authenticate, agendamentoController.listarAgendamentosConcluidos);
 router.get('/historico', authenticate, agendamentoController.listarAgendamentosHistorico);
 router.patch('/:id/status', authenticate, agendamentoController.atualizarStatusAgendamento);
 
