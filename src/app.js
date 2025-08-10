c34fb74ec54164d3728267955256a51fca9e171c
Nailan-Nobre <nailannobre22@gmail.com>
2025-08-10 07:54:28 -0300

    ...

--- src/app.js
+++ src/app.js
@@ -13,8 +13,8 @@ const app = express();
 
 // Middlewares
 app.use(cors());
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ extended: true, limit: '20mb' }));
 
 // Middleware de logging para debug
 app.use((req, res, next) => {
